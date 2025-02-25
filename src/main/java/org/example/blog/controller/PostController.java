13c27966e8ddc39aaa3f2e8018d936d6f286ea75
Bolohonov <bolohonov@list.ru>
2025-02-25 17:23:49 +0400

    Init commit

---
+++ src/main/java/org/example/blog/controller/PostController.java
@@ -0,0 +1,19 @@
+package org.example.blog.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/blog")
+@RequiredArgsConstructor
+public class PostController {
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String homePage() {
+        return "<h1>Hello, world!</h1>";
+    }
+}
