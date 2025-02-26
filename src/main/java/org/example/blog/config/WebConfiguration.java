3276d0849e9a9c4220f937621752d22876e616a3
bolohonov <bolohonov@list.ru>
2025-02-26 21:49:26 +0400

    refactoring

--- src/main/java/org/example/blog/config/WebConfiguration.java
+++ src/main/java/org/example/blog/config/WebConfiguration.java
@@ -7,5 +7,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "org.example.blog.api.controller")
+@ComponentScan(basePackages = "org.example.blog.controller")
 public class WebConfiguration implements WebMvcConfigurer {}
